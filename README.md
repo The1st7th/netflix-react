b510d5c13d98558bc0b11d3c81230e16b87e7c28
1st <eric.e.nicolas@gmail.com>
2018-07-08 13:46:15 -0700

    added readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# React-app
+
+Webapp to add movies to a list and diplay them
+
+
+### Prerequisites
+
+nodejs, react, webpack, react-bootstrap, reactstrap
+
+```
+npm install
+```
+## Component tree
+
+[![react_1.png](https://s26.postimg.cc/7fmt2eiax/react_1.png)](https://postimg.cc/image/fxw96qoth/)
+
+
+## Authors
+
+Eric nicolas
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* none
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -38,6 +38,17 @@ function NewMovie(props){
     <button type ="button" className="btn btn-danger" >Movie List</button>
   </div>
   </Link>
+  <p>
+    copy paste this in the forms</p>
+    <p>The Shawshank Redemption</p>
+    <p>Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.</p>
+    <p>https://image.tmdb.org/t/p/w500/9O7gLzmreU0nGkIB6K3BsJbzvNv.jpg</p>
+    <p> the GodFather </p>
+    <p> Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.</p>
+    <p>https://image.tmdb.org/t/p/w500/rPdtLWNsZmAtoZl9PK7S2wE3qiS.jpg</p>
+    <p> Your Name </p>
+    <p>High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places. Mitsuha wakes up in Taki’s body, and he in hers. This bizarre occurrence continues to happen randomly, and the two must adjust their lives around each other.</p>
+    <p>https://image.tmdb.org/t/p/w500/xq1Ugd62d23K2knRUx6xxuALTZB.jpg</p>
 </div>
     );
 }
